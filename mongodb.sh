607147e916709dab91e071fb76cf0de4db3b38b0
madhav <chilupoori.reddy@opstree.com>
2023-11-28 23:38:17 +0530

    c1

--- mongodb.sh
+++ mongodb.sh
@@ -1,7 +1,7 @@
-set_location=${pwd}
+script_location=${pwd}
 
-cp ${set_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo
-dnf install mongodb-org -y
+cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo
+yum install mongodb-org -y
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 systemctl enable mongod 
 systemctl start mongod 
