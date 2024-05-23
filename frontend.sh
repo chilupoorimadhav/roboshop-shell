74a602ad752b25b3f6147d3fca0e752d7f131e10
Madhav Chilupoori <madhav_chilupoori@epam.com>
2024-05-23 20:01:41 +0530

    first commit

--- frontend.sh
+++ frontend.sh
@@ -1,12 +1,13 @@
+script_location =$(pwd)
 yum install nginx -y
-systemctl enable nginx
-systemclt start nginx
 rm -rf /usr/share/nginx/html/*
 
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
-cp files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
+cp $script_location files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
+systemctl enable nginx
+systemclt start nginx
 
 systemctl restart nginx
